bdf2435f9d6c81a995dcf9714fe375cdb39e4409
SonGlad <okoshevy@gmail.com>
2024-10-24 01:13:45 +0300

    Update Section.styled.js

--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -3,21 +3,27 @@ import { styled } from "styled-components";
 
 export const HeroSectionStyle = styled.section`
     width: 100%;
-    position: relative;
-    z-index: 1;
+    @media screen and (min-width: 768px){
+        position: relative;
+        z-index: 1;
+    }
 `
 
 
 export const AboutSectionStyled = styled.section`
     width: 100%;
-    position: relative;
-    z-index: 1;
+    @media screen and (min-width: 768px){
+        position: relative;
+        z-index: 1;
+    }
 `
 
 export const SectionStyled = styled.section`
     width: 100%;
-    position: relative;
-    z-index: 1;
+    @media screen and (min-width: 768px){
+        position: relative;
+        z-index: 1;
+    }
 `
 
 export const ContactSectionStyled = styled.section`
