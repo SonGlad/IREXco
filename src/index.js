43f24c484313f57979418bbda56438c8b514c56a
SonGlad <okoshevy@gmail.com>
2024-10-24 17:57:43 +0300

    routes

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   // <React.StrictMode>
-    <BrowserRouter basename='/IREXco'>
+    <BrowserRouter>
       <ThemeProvider theme={theme}>
         <GlobalStyle/>
           <App />
